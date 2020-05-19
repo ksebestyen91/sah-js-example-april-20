f6fd6cba98f7c6eb9a1302dd3d18b275ff8ce26c
ksebestyen91 <64491691+ksebestyen91@users.noreply.github.com>
2020-05-19 05:13:40 +0200

    add jquery first

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+// Third part:
+
+$('h1').html('This is done by <em>JavaScript!</em>');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+$('h2').toggleClass('kek');
+
 console.log('Script file is loaded.');
+
+// Single line comment
+
+/*  
+Multiple
+lines 
+of comment
+*/
